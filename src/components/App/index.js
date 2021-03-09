459fd238a0f6f605fe83330b5b80b816e6c88dce
Nazar <wolfter12@gmail.com>
2021-03-09 23:16:26 +0200

    Added TrackerInput component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header';
+import TrackerInput from '../TrackerInput';
 
 function App() {
   return (
     <div>
       <Header />
+      <TrackerInput />
     </div>
   );
 }
