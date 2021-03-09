597069b6e52d4618a8010d5121cc58bd2b5baf1a
Nazar <wolfter12@gmail.com>
2021-03-09 23:13:29 +0200

    Added Header component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../Header';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
