4e56c2e1098a2b1ac991918cbd7c1a4fccccbcca
Nazar <wolfter12@gmail.com>
2021-03-09 23:16:09 +0200

    Created TrackerInput component

---
+++ src/components/TrackerInput/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function TrackerInput() {
+  return (
+    <div>
+      <input type="text" />
+      <button>start</button>
+    </div>
+  )
+}
+
+export default TrackerInput;
