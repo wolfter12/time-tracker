12b90f9c87f2156bf7a3232fde1839242590e349
Nazar <wolfter12@gmail.com>
2021-03-10 21:38:20 +0200

    Updated TrackingList component

--- src/components/TrackingList/index.js
+++ src/components/TrackingList/index.js
@@ -1,24 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import moment from 'moment';
-import 'moment-duration-format';
+
+import Tracker from '../Tracker';
 
 function TrackingList({ trackers }) {
-  return (
-    <div>
-      {trackers.map(({ id, name, duration, breakpoint, paused }) => {
-        const currentDuration = moment.duration(duration);
-        const seconds = `00${currentDuration.get('seconds')}`.slice(-2);
-        const minutes = `00${currentDuration.get('minutes')}`.slice(-2);
-        const hours = `00${currentDuration.get('hours')}`.slice(-2);
-        return (
-          <div key={id} style={{ display: 'flex' }}>
-            <h3>{name}</h3>
-            <span>{`${hours}:${minutes}:${seconds}`}</span>
-          </div>
-        );
-      })}
-    </div>
-  );
+  const trackersList = trackers.map((tracker) => {
+    return <Tracker key={tracker.id} tracker={tracker} />;
+  });
+  return <div>{trackersList}</div>;
 }
 
 export default TrackingList;
