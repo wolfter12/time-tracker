0c69e08a4b6ad0c7a6cb88f32442fe78b20d1a65
Nazar <wolfter12@gmail.com>
2021-03-10 20:45:34 +0200

    Updated Tracker component props

--- src/components/TrackerInput/index.js
+++ src/components/TrackerInput/index.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import moment from 'moment';
+import React from 'react';
 
-export function TrackerInput() {
-  const [name, setName] = useState('');
-
-  const onInputHandler = (e) => {
-    setName(e.target.value);
-  };
-
-  const addNewTracker = () => {
-    const data = {
-      id: uuidv4(),
-      name: name,
-      breakpoint: moment(),
-      duration: moment.duration(0, 'seconds').toJSON(),
-      paused: false,
-    };
-    const appData = localStorage.getItem('trackers');
-    const trackers = appData !== null ? JSON.parse(appData) : [];
-    localStorage.setItem('trackers', JSON.stringify([...trackers, data]));
-    setName('');
-  };
-
+function TrackerInput({ trackerName, onInputHandler, onButtonHandler }) {
   return (
     <div>
-      <input type="text" value={name} onInput={onInputHandler} />
-      <button onClick={addNewTracker}>start</button>
+      <input type="text" value={trackerName} onInput={onInputHandler} />
+      <button onClick={onButtonHandler}>start</button>
     </div>
   );
 }
