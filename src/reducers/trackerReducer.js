b70311dc96c24f520f1e5303b04163e4677453bc
Nazar <wolfter12@gmail.com>
2021-03-11 12:56:09 +0200

    Created trackerReducer

---
+++ src/reducers/trackerReducer.js
@@ -0,0 +1,20 @@
+import { ADD_TRACKER, DELETE_TRACKER, GET_TRACKERS } from '../actions/types';
+
+const initialState = {
+  items: [],
+  item: {},
+};
+
+const trackerReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_TRACKERS:
+      return {
+        ...state,
+        items: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default trackerReducer;
