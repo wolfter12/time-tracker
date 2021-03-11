02413cde5d2c3ce0884267cccd9dae515d2507c5
Nazar <wolfter12@gmail.com>
2021-03-11 12:56:32 +0200

    Created trackerActions

---
+++ src/actions/trackersActions.js
@@ -0,0 +1,27 @@
+import { ADD_TRACKER, DELETE_TRACKER, GET_TRACKERS } from './types';
+
+const TRACKERS = 'trackers';
+
+export const getTrackers = () => (dispatch) => {
+  console.log('getting trackers');
+  try {
+    const appData = localStorage.getItem(TRACKERS);
+    if (appData !== null) {
+      const localTrackers = JSON.parse(appData);
+      if (Array.isArray(localTrackers)) {
+        dispatch({
+          type: GET_TRACKERS,
+          payload: localTrackers,
+        });
+      }
+    }
+
+    // const localTrackers = JSON.parse(localStorage.getItem(TRACKERS));
+    // dispatch({
+    //   type: GET_TRACKERS,
+    //   payload: localTrackers,
+    // });
+  } catch (error) {
+    console.error(error);
+  }
+};
