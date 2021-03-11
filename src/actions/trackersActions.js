8263a7521f6769c794f4fd48d63696ddd59f0ff7
Nazar <wolfter12@gmail.com>
2021-03-11 17:03:32 +0200

    Added TODO

--- src/actions/trackersActions.js
+++ src/actions/trackersActions.js
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
 import { ADD_TRACKER, DELETE_TRACKER } from './types';
 
+// TODO: Check if it is better to move logic in first argument
+// https://medium.com/javascript-scene/10-tips-for-better-redux-architecture-69250425af44#dd39
 export const addTracker = (name) => (dispatch) => {
   const now = moment();
   const newTracker = {
