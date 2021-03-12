48491b3fc93f437f2c867e1b1af5df427075df67
Nazar <wolfter12@gmail.com>
2021-03-12 13:05:57 +0200

    Updated Duration component file structure

--- src/components/Duration/index.js
+++ src/components/Duration/index.js
@@ -1,18 +1,2 @@
-import React from 'react';
-import moment from 'moment';
-import style from './Duration.module.css';
-
-function Duration({ duration }) {
-  const currentDuration = moment.duration(duration);
-  const seconds = `00${currentDuration.get('seconds')}`.slice(-2);
-  const minutes = `00${currentDuration.get('minutes')}`.slice(-2);
-  // TODO: Format hours output more precisely
-  const hours = `00${Math.floor(currentDuration.asHours())}`.slice(-2);
-  return (
-    <div className={style.stopwatch}>
-      <span>{`${hours}:${minutes}:${seconds}`}</span>
-    </div>
-  );
-}
-
+import Duration from './Duration';
 export default Duration;
