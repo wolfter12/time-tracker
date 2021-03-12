3daf29c3e2f63c3426c18730ff6661c19eece63c
Nazar <wolfter12@gmail.com>
2021-03-12 13:06:52 +0200

    Updated Header component files structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,2 @@
-import React from 'react';
-import style from './Header.module.css'
-
-function Header() {
-  return (
-    <div className={style.header}>
-      <h1>tracker</h1>
-    </div>
-  );
-}
-
+import Header from './Header';
 export default Header;
