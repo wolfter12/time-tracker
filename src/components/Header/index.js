0a956586e7e7f50c27eeec3e631f3138738908be
Nazar <wolfter12@gmail.com>
2021-03-12 12:53:51 +0200

    Created Header css module + added to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Header.module.css'
 
 function Header() {
   return (
-    <div>
+    <div className={style.header}>
       <h1>tracker</h1>
     </div>
   );
