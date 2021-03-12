957a6235d5410206e82cf63fd8cf52a953ae3b94
Nazar <wolfter12@gmail.com>
2021-03-12 13:01:34 +0200

    Created TrackerControl css module

---
+++ src/components/TrackerControl/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import StopwatchControlButton from '../StopwatchControlButton';
+import DeleteButton from '../DeleteButton';
+import style from './TrackerControl.module.css';
+
+function TrackerControl({ id, paused, breakpoint, duration }) {
+  return (
+    <div className={style.Control}>
+      <StopwatchControlButton
+        id={id}
+        paused={paused}
+        breakpoint={breakpoint}
+        duration={duration}
+      />
+      <DeleteButton id={id} />
+    </div>
+  );
+}
+
+export default TrackerControl;
