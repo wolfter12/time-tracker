ea8110580ee0becf50a96eaa6bf4445220e917c1
Nazar <wolfter12@gmail.com>
2021-03-13 17:35:37 +0200

    Added background color for running trackers

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -6,8 +6,9 @@ import style from './Tracker.module.css';
 function Tracker({ tracker }) {
   const { id, name, paused, breakpoint, duration } = tracker;
   const color = paused ? style.stopped : style.running;
+  const bg = paused ? '' : style['bg-running'];
   return (
-    <div className={style.tracker}>
+    <div className={`${style.tracker} ${bg}`}>
       <h3 className={`${style.name} ${color}`}>{name}</h3>
       <Duration duration={duration} paused={paused} />
       <TrackerControl
