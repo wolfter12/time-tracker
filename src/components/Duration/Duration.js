9884d0c8ecfbcf16a725eb113856a10cdb3adf3f
Nazar <wolfter12@gmail.com>
2021-03-13 20:58:33 +0200

    Added formatDuration funcition

--- src/components/Duration/Duration.js
+++ src/components/Duration/Duration.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import moment from 'moment';
 import style from './Duration.module.css';
+import formatDuration from '../../functions/formatDuration';
 
 function Duration({ duration, paused }) {
-  const currentDuration = moment.duration(duration);
-  const seconds = `00${currentDuration.get('seconds')}`.slice(-2);
-  const minutes = `00${currentDuration.get('minutes')}`.slice(-2);
-  const hoursTotal = Math.floor(currentDuration.asHours()).toString();
-  const hours = hoursTotal.length >= 2
-    ? hoursTotal
-    : hoursTotal.length === 1
-      ? `0${hoursTotal}`
-      : '00';
+  const formattedDuration = formatDuration(duration);
 
   const { stopwatch, stopped, running } = style;
   const color = paused ? stopped : running;
-  
+
   return (
     <div className={`${stopwatch} ${color}`}>
-      <span>{`${hours}:${minutes}:${seconds}`}</span>
+      <span>{formattedDuration}</span>
     </div>
   );
 }
