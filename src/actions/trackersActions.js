85e716761c6e6769e9486e2d19ce754bc6bb1bd6
Nazar <wolfter12@gmail.com>
2021-03-13 20:41:59 +0200

    Removed updateStopwatches action creator

--- src/actions/trackersActions.js
+++ src/actions/trackersActions.js
@@ -49,24 +49,3 @@ export const changeStopwatchState = (id, paused, breakpoint, duration) => (
     },
   });
 };
-
-export const updateStopwatches = () => (dispatch, getState) => {
-  const updatedTrackers = getState().trackers.map((tracker) => {
-    if (tracker.paused) {
-      return tracker;
-    }
-    const prevBreakpoint = moment(tracker.breakpoint);
-    const prevDuration = moment.duration(tracker.duration);
-    const nextBreakpoint = moment();
-    const nextDuration = prevDuration.add(nextBreakpoint.diff(prevBreakpoint));
-    return {
-      ...tracker,
-      breakpoint: nextBreakpoint.toJSON(),
-      duration: nextDuration.toJSON(),
-    };
-  });
-  dispatch({
-    type: UPDATE_STOPWATCHES,
-    updatedTrackers,
-  });
-};
