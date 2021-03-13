b9e47b63fdae282bb4878ce73fae758d948ea950
Nazar <wolfter12@gmail.com>
2021-03-13 17:01:25 +0200

    Deleted redundant actiona creator and reducer

--- src/actions/controlActions.js
+++
@@ -1,8 +0,0 @@
-import { CHANGE_STOPWATCH_STATE } from './types';
-
-export const changeStopwatchState = (id, paused) => (dispatch) => {
-  dispatch({
-    type: CHANGE_STOPWATCH_STATE,
-    payload: { id, paused: !paused },
-  });
-};
