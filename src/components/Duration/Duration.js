d2afe936672bd1f05db32c20378d9f7000c0f06c
Nazar <wolfter12@gmail.com>
2021-03-13 16:55:59 +0200

    Added conditional color changing

--- src/components/Duration/Duration.js
+++ src/components/Duration/Duration.js
@@ -2,14 +2,15 @@ import React from 'react';
 import moment from 'moment';
 import style from './Duration.module.css';
 
-function Duration({ duration }) {
+function Duration({ duration, paused }) {
   const currentDuration = moment.duration(duration);
   const seconds = `00${currentDuration.get('seconds')}`.slice(-2);
   const minutes = `00${currentDuration.get('minutes')}`.slice(-2);
   // TODO: Format hours output more precisely
   const hours = `00${Math.floor(currentDuration.asHours())}`.slice(-2);
+  const color = paused ? style.stopped : style.running;
   return (
-    <div className={style.stopwatch}>
+    <div className={`${style.stopwatch} ${color}`}>
       <span>{`${hours}:${minutes}:${seconds}`}</span>
     </div>
   );
